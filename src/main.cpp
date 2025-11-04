ed80630ce28f8f1d4d196a8a51b72f9d908467e5
alchemystdevs <ing.alejandronc@outlook.com>
2025-11-03 18:10:29 -0600

    usable mac adrees ole viewer

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
+#include <esp_system.h>
 
-// put function declarations here:
-int myFunction(int, int);
+namespace
+{
+  constexpr uint32_t kSerialBaud = 115200;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  String buildDeviceId()
+  {
+    uint8_t mac[6] = {0};
+    esp_read_mac(mac, ESP_MAC_WIFI_STA);
+
+    char suffix[7]; // six hex digits + null terminator
+    snprintf(suffix, sizeof(suffix), "%02X%02X%02X", mac[3], mac[4], mac[5]);
+
+    return String("OLEO_") + suffix;
+  }
+} // namespace
+
+void setup()
+{
+  Serial.begin(kSerialBaud);
+  delay(500); // give USB CDC time to enumerate
+
+  const String deviceId = buildDeviceId();
+  Serial.println();
+  Serial.print("Device ID: ");
+  Serial.println(deviceId);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // Nothing else to do; keep the MCU alive.
+  delay(1000);
 }
